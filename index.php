2019e1bf56ece2f2b168f6cab0d84a37399d6110
Nyolczas <nyolczas@gmail.com>
2017-12-06 00:09:28 +0100

    8i főoldal

--- index.php
+++ index.php
@@ -1,49 +1,53 @@
 <?php
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="hu">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Nyolczas István </title>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nyolczas István </title>
+    <!-- css reset -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css">
+    <!-- my styles -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-  <header>
-    <div class="headerbox">
-      <button class="hamburger">&#9776;</button>
-      <button class="cross">&#735;</button>
-      <div id="menu">
-        <ul>
-          <a href="pricelist/index.php">
-            <li>ÜGYFÉL BELÉPÉS</li>
-          </a>
-          <a href="#">
-            <li>LINK TWO</li>
-          </a>
-          <a href="#">
-            <li>LINK THREE</li>
-          </a>
-          <a href="#">
-            <li>LINK FOUR</li>
-          </a>
-          <a href="#">
-            <li>LINK FIVE</li>
-          </a>
-        </ul>
-      </div>
-      <img src="img\profil.jpg" alt="8-i profil">
-      <h1>Nyolczas István</h1>
-      <h2>Webfejlesztő - Grafikus - Animátor</h2>
-    </div>
-  </header>
-  <p id="demo"></p>
+    <header>
+        <div class="headerbox">
+            <button class="hamburger">&#9776;</button>
+            <button class="cross">&#735;</button>
+            <div id="menu">
+                <ul>
+                    <a href="pricelist/index.php">
+                        <li>FREELUSION Pricelist</li>
+                    </a>
+                    <a href="#">
+                        <li>LINK TWO</li>
+                    </a>
+                    <a href="#">
+                        <li>LINK THREE</li>
+                    </a>
+                    <a href="#">
+                        <li>LINK FOUR</li>
+                    </a>
+                    <a href="#">
+                        <li>LINK FIVE</li>
+                    </a>
+                </ul>
+            </div>
+            <img src="img\profil.jpg" alt="8-i profil">
+            <h1>Nyolczas István</h1>
+            <h2>Webfejlesztő - Grafikus - Animátor</h2>
+        </div>
+    </header>
+    <p id="demo"></p>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="js/time-countdown.js"></script>
-  <script src="js/menu.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/time-countdown.js"></script>
+    <script src="js/menu.js"></script>
 </body>
 
 </html>
