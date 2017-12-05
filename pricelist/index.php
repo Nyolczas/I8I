a131218d77607117de69da323f7e49156d9b4d1a
Nyolczas <nyolczas@gmail.com>
2017-12-05 16:44:50 +0100

    pricelist login

--- pricelist/index.php
+++ pricelist/index.php
@@ -46,7 +46,7 @@ if (isset($_POST['rendben'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Ügyfél Belépés</title>
+        <title>Pricelist Sign in</title>
         <!--    Bootstrap-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <!-- Custom styles for this template -->
