eee828224cf652dad403f12dde4518229ee97459
Nyolczas <nyolczas@gmail.com>
2017-12-05 18:26:02 +0100

    pricelist intro

--- pricelist/introdution.php
+++ pricelist/introdution.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['belepett'])) {
         <meta name="keywords" content="Freelusion, Video Mapping Studio, Dance Company, Show Factory, Visual Studio">
         <meta name="description" content="Freelusion, Video Mapping Studio, Dance Company, Show Factory, Visual Studio">
         <title>Introduton | FREELUSION Price List</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <style>
             html,
             body {
@@ -34,9 +35,20 @@ if (!isset($_SESSION['belepett'])) {
     </head>
 
     <body class="hatter">
-        <div class="tokkozepre">
-
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-7">
+                    <a class="btn btn-primary" href="#" role="button">Dance Shows</a>
+                    <a class="btn btn-primary" href="#" role="button">Video Projection Solutions</a>
+                    <a class="btn btn-primary" href="#" role="button">100% Freelusion</a>
+                    <a class="btn btn-primary" href="#" role="button">Animation Artworks</a>
+                </div>
+            </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 
     </html>
