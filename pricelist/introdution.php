24a92f1de3831a5eef1f5d19f1fe0d1ee6e99136
Nyolczas <nyolczas@gmail.com>
2017-12-12 11:23:20 +0100

    intro

--- pricelist/introdution.php
+++ pricelist/introdution.php
@@ -79,48 +79,51 @@ if (!isset($_SESSION['belepett'])) {
             </div>
         </nav>
         <!-- /.navbar -->
-            <!-- Az oldal tartalma -->
+        <!-- Az oldal tartalma -->
 
-            <div class="bgimg-1">
-                <div class="caption">
-                    <span class="border">Introduction</span>
-                </div>
+        <div class="bgimg-1">
+            <div class="caption">
+                <span class="border">Introduction</span>
             </div>
+        </div>
 
-            <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+        <div class="txt-block">
+            <div class="container">
                 <p>
                     Freelusion is the biggest - with 20 inhouse, full-time specialists - and most experienced graphic design studio specialized for, but not limited to video mapping solutions.
                 </p>
             </div>
+        </div>
 
-            <div class="bgimg-2">
-                <div class="caption">
-                    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">LESS HEIGHT</span>
-                </div>
+        <div class="bgimg-2">
+            <div class="caption">
+                <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">WE ARE THE BIGGEST</span>
             </div>
+        </div>
 
-            <div style="position:relative;">
-                <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-                   <div class="container">
-                       <p>
+        <div style="position:relative;">
+            <div class="txt-block">
+                <div class="container">
+                    <p>
                         We are particularly proud of our exclusive clientele and the unique 3D spectacles we have put on stage and on various canvases throughout the years. Since 2009, we have had over 200 shows in more than 40 countries on 5 continents.
                     </p>
                     <p>
                         With more than 20 existing short shows and 3 different full night shows plus 50+ unique tailor-made corporate shows in our arsenal, we are the Planet’s very first and most experienced 3D dance company. Why choose any if you can have the best?
                     </p>
-                   </div>
-                    
                 </div>
-            </div>
 
-            <div class="bgimg-3">
-                <div class="caption">
-                    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">SCROLL UP</span>
-                </div>
             </div>
+        </div>
 
-            <div style="position:relative;">
-                <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+        <div class="bgimg-3">
+            <div class="caption">
+                <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">YOU CAN HAVE THE BEST</span>
+            </div>
+        </div>
+
+        <div style="position:relative;">
+            <div class="txt-block">
+                <div class="container">
                     <p>
                         Our globetrotting dance company has been on primetime talent shows such as America’s Got Talent (Best Dance Act at Season 10), Britain’s Got Talent (Viewers’ Choice 3rd Prize), and Fake Off (Finalists), Arabs Got Talent (Finalists), Minita TV Show (Finalists) and has been invited to dozens of other TV productions in Europe and Asia.
                     </p>
@@ -129,13 +132,14 @@ if (!isset($_SESSION['belepett'])) {
                     </p>
                 </div>
             </div>
+        </div>
 
-            <div class="bgimg-1">
-                <div class="caption">
-                    <span class="border">COOL!</span>
-                </div>
+        <div class="bgimg-1">
+            <div class="caption">
+                <span class="border">COOL!</span>
             </div>
-            <!-- / Az oldal tartalma -->
+        </div>
+        <!-- / Az oldal tartalma -->
 
         <!-- footer -->
         <footer class="footer bg-dark">
