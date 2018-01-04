14afe20f197fb7ff27b413744b61ca66a44c3b58
Nyolczas <nyolczas@gmail.com>
2018-01-04 21:57:09 +0100

    Robot gallery

--- js/slide-panels.js
+++ js/slide-panels.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $("#miss-u-flip").click(function () {
         $("#miss-u-panel").slideToggle("slow");
     });
+    $("#robot-flip").click(function () {
+        $("#robot-panel").slideToggle("slow");
+    });
 });
